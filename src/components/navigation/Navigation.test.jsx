b09707ee0f8beaec6e4875587c8213bb6bef4953
Fernando Sibarani <fernandosibarani45@gmail.com>
2023-12-29 22:44:15 +0700

    fix: set ignore case to false

---
+++ src/components/navigation/Navigation.test.jsx
@@ -0,0 +1,74 @@
+import {describe, it, expect, afterEach} from 'vitest';
+import {cleanup, render} from '@testing-library/react';
+import Navigation from './Navigation';
+import {MemoryRouter} from 'react-router-dom';
+
+/**
+ * Testing scenario
+ *
+ * - Navigation component
+ *  - should show the navigation
+ *  - should show the leaderboards active navigation
+ *  - should show the profile active navigation
+ */
+
+describe('Navigation component', () => {
+  afterEach(() => {
+    cleanup();
+  });
+
+  it('should show the navigation', () => {
+    // Arrange
+    const {container} = render(
+        <MemoryRouter initialEntries={['/']}>
+          <Navigation />
+        </MemoryRouter>,
+    );
+    const component = container.querySelector('nav');
+
+    // Assert
+    expect(component).toBeTruthy();
+  });
+
+  it('should show the leaderboards active navigation', async () => {
+    // Arrange
+
+    const {container} = render(
+        <MemoryRouter initialEntries={['/leaderboards']}>
+          <Navigation />
+        </MemoryRouter>,
+    );
+
+    // Assert
+    expect(
+        container.querySelector('nav li a[href="/leaderboards"]').closest('li'),
+    ).toHaveClass('active');
+    expect(
+        container.querySelector('nav li a[href="/"]').closest('li'),
+    ).not.toHaveClass('active');
+    expect(
+        container.querySelector('nav li a[href="/profile"]').closest('li'),
+    ).not.toHaveClass('active');
+  });
+
+  it('should show the profile active navigation', async () => {
+    // Arrange
+
+    const {container} = render(
+        <MemoryRouter initialEntries={['/profile']}>
+          <Navigation />
+        </MemoryRouter>,
+    );
+
+    // Assert
+    expect(
+        container.querySelector('nav li a[href="/profile"]').closest('li'),
+    ).toHaveClass('active');
+    expect(
+        container.querySelector('nav li a[href="/"]').closest('li'),
+    ).not.toHaveClass('active');
+    expect(
+        container.querySelector('nav li a[href="/leaderboards"]').closest('li'),
+    ).not.toHaveClass('active');
+  });
+});
