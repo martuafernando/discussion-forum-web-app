78f1258e1d507d83eea276a80423971b52c0212a
Fernando Sibarani <fernandosibarani45@gmail.com>
2023-12-29 22:49:38 +0700

    test: make failed test into success test

--- src/components/category-list/CategoryList.test.jsx
+++ src/components/category-list/CategoryList.test.jsx
@@ -49,7 +49,7 @@ describe('CategoryList component', () => {
     const categoryItemActive = container.querySelector('.category-item.active');
 
     // Assert
-    expect(categoryItemActive).toBeFalsy();
+    expect(categoryItemActive).toBeTruthy();
   });
 
   it('should remove class active when active category clicked', async () => {
